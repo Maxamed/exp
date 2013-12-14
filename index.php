944edba8bbda4962e710280a7e4739969c06d515
Mohamed Jama <mj@mjama.com>
2013-12-14 21:01:36 +0000

    Added Foundation Zurb and converted everything to php

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php include 'includes/header.php'; ?>
+
+
+
+
+        <div class="row">
+    <div class="large-12 columns">
+ 
+<?php include 'includes/navigation.php'; ?>
+ 
+      <div class="row"> 
+ 
+ 
+    <!-- Thumbnails -->
+ 
+        <div class="large-12 columns">
+          <div class="row">
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/1000x1000&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=Thumbnail">
+ 
+              <div class="panel">
+                <h5>Item Name</h5>
+                <h6 class="subheader">$000.00</h6>
+              </div>
+            </div>
+          </div>
+ 
+    <!-- End Thumbnails -->
+ 
+ 
+          </div>
+        </div>
+      </div>
+ 
+ 
+    <!-- Footer -->
+ 
+      <footer class="row">
+        <div class="large-12 columns"><hr />
+          <div class="row">
+ 
+            <div class="large-6 columns">
+              <p>© Copyright no one at all. Go to town.</p>
+            </div>
+ 
+            <div class="large-6 columns">
+              <ul class="inline-list right">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+              </ul>
+            </div>
+ 
+          </div>
+        </div>
+      </footer>
+ 
+    <!-- End Footer -->
+ 
+    </div>
+  </div>
+
+<?php include 'includes/footer.php'; ?>
